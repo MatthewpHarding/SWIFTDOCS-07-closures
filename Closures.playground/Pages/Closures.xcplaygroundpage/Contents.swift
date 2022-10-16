ce2952d51725e6f9570acb1fc16180169086c075
Matthew Harding <matthewpharding@gmail.com>
2022-10-16 11:08:34 +0100

    Added the page number (from official Swift documentation) to each Xcode playground file.

--- Closures.playground/Pages/Closures.xcplaygroundpage/Contents.swift
+++ Closures.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: ![Swift](swift-logo.png)
 //: Swift v5.7 [official docs](https://docs.swift.org/swift-book/LanguageGuide/Closures.html)
-//: # Closures
+//: # 7. Closures
 //:
 //:
 //: Closures are self-contained blocks of functionality that can be passed around and used in your code. Closures in Swift are similar to blocks in C and Objective-C and to lambdas in other programming languages.
